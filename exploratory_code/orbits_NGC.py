72b03c6775f06227ffddf3d8413571ba58af6391
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-02-22 10:39:54 -0700

    tested code!

--- exploratory_code/orbits_NGC.py
+++ exploratory_code/orbits_NGC.py
@@ -30,7 +30,7 @@ def NGC_Sgr_LMC_orbit(time, Host_model, disk_params,  Sag_model, LMC_model,\
     t5, posLMC, velLMC, posMWsLMC, velMWsLMC, posNGCsLMC, velNGCsLMC,\
     posSagLMC, velSagLMC = soda.leapfrog.integrate_sat(time, pos_host,\
     vel_host, Host_model, disk_params, bulge_params,alpha=[0, 0.3],\
-    dt=0.1, pos_sat=LMC_IC_pos,\
+    dt=0.001, pos_sat=LMC_IC_pos,\
     vel_sat=LMC_IC_vel, satellite_model=LMC_model, pos_p=NGC_IC_pos,\
     vel_p=NGC_IC_vel, pos_sat2=Sag_IC_pos,\
     vel_sat2=Sag_IC_vel, \
@@ -43,7 +43,7 @@ def NGC_Sgr_LMC_orbit(time, Host_model, disk_params,  Sag_model, LMC_model,\
     velSag_GLMC = galactocentic(velMWsLMC, velSagLMC)
     velNGC_GLMC = galactocentic(velMWsLMC, velNGCsLMC)
     velLMC_GLMC = galactocentic(velMWsLMC, velLMC)
-    
+
     del(Sag_model)
 
     return t5, posNGC_GLMC, velNGC_GLMC, posSag_GLMC, velSag_GLMC,\
@@ -80,7 +80,7 @@ def output_file(filename, time, posNGC, velNGC, posSag, velSag, rel, **kwargs):
             posSag[i,1], posSag[i,2], velSag[i,0], velSag[i,1],\
             velSag[i,2], posLMC[i,0], posLMC[i,1], posLMC[i,2], \
             velLMC[i,0], velLMC[i,1], velLMC[i,2], rel[i,0],\
-            rel[i,1], rel[1,2]))
+            rel[i,1], rel[i,2]))
         f.close()
     else:
         f = open(filename, 'w')
@@ -90,11 +90,11 @@ def output_file(filename, time, posNGC, velNGC, posSag, velSag, rel, **kwargs):
             %(time[i], posNGC[i,0], posNGC[i,1], posNGC[i,2],\
             velNGC[i,0], velNGC[i,1], velNGC[i,2], posSag[i,0],\
             posSag[i,1], posSag[i,2], velSag[i,0], velSag[i,1],\
-            velSag[i,2], rel[i,0], rel[i,1], rel[1,2]))
+            velSag[i,2], rel[i,0], rel[i,1], rel[i,2]))
         f.close()
 if __name__ == "__main__":
 
-    time = 4
+    T = 4
 
     pos_host = [0,0,0]
     vel_host = [0,0,0]
@@ -120,63 +120,64 @@ if __name__ == "__main__":
     sgr_vel = np.array([242.5, 5.6, 228.1])
 
 
-    ics = np.loadtxt('mc2.ngc2419')
+    #ics = np.loadtxt('mc2.ngc2419')
 
-    pos = [[i,j,k] for i,j,k in zip(ics[:,2], ics[:,3], ics[:,4])]
-    vel = [[i,j,k] for i,j,k in zip(ics[:,5], ics[:,6], ics[:,7])]
+    #pos = [[i,j,k] for i,j,k in zip(ics[:,2], ics[:,3], ics[:,4])]
+    #vel = [[i,j,k] for i,j,k in zip(ics[:,5], ics[:,6], ics[:,7])]
 
-    assert(len(pos)==10000)
-    assert(len(vel)==10000)
+    #assert(len(pos)==10000)
+    #assert(len(vel)==10000)
 
-    #pos_NGC2419 = [-87.43, -0.51, 37.31]
-    #vel_NGC2419 = [16.55, 48.46, -31.33]
+    pos = [-87.43, -0.51, 37.31]#NGC2419
+    vel = [16.55, 48.46, -31.33]#NGC2419
 
     #pos_NGC2419_2 = [-79.1-8.3, -0.5, 37.4-0.014]
     #vel_NGC2419_2 = [-32.6+11.1, -177.2+240.24, -119.3+7.25]
 
 
     LMC_model =  ['hernquist', LMC_Ms[0], LMC_as[0]]
+
     lmc_pos = [-1, -41, -28]
     lmc_vel = [-57, -226, 221]
 
+    satellite_model_sgr2 = ['NFW', 1E10, 44, 8]
     if sys.argv[1] == 'MW':
         print('orbtis around the MW')
 
     elif sys.argv[1] == 'MWlSgr':
-        satellite_model_sgr2 = ['NFW', 1E10, 44, 8]
+        for i in range(len(pos)):
+            time, posNGC, velNGC, posSag, velSag, relNGCSAG\
+            = NGC_Sgr_orbit(T, host_model_l, disk_params_h,\
+            satellite_model_sgr2, pos[i], vel[i], sgr_pos, sgr_vel)
 
-        time, posNGC, velNGC, posSag, velSag, relNGCSAG\
-        = NGC_Sgr_orbit(time, host_model_l, disk_params_h,\
-        satellite_model_sgr2, pos_NGC2419, vel_NGC2419, sgr_pos, sgr_vel)
-        output_file(time, posNGC, velNGC, posSag, velSag, relNGCSAG)
+            output_file('MWlSgr_ICs{}.txt'.format(str(i)), time, posNGC,\
+            velNGC, posSag, velSag, relNGCSAG)
 
 
     elif sys.argv[1] == 'MWhSgr':
-        satellite_model_sgr2 = ['NFW', 1E10, 44, 8]
+        for i in range(len(pos)):
+            time, posNGC, velNGC, posSag, velSag, relNGCSAG\
+            = NGC_Sgr_orbit(T, host_model_h, disk_params_l,\
+            satellite_model_sgr2, pos[i], vel[i], sgr_pos, sgr_vel)
 
-        time, posNGC, velNGC, posSag, velSag, relNGCSAG\
-        = NGC_Sgr_orbit(time, host_model_h, disk_params_l,\
-        satellite_model_sgr2, pos_NGC2419, vel_NGC2419, sgr_pos, sgr_vel)
-        output_file(time, posNGC, velNGC, posSag, velSag, relNGCSAG)
+            output_file('MWhSgr_ICs{}.txt'.format(str(i)), time, posNGC, velNGC, posSag, velSag, relNGCSAG)
 
 
     elif sys.argv[1] == 'MWlSgrLMC':
         for i in range(len(pos)):
-            satellite_model_sgr2 = ['NFW', 1E10, 44, 8]
             time, posNGC, velNGC, posSag, velSag, posLMC, velLMC, \
-            relNGCSAG = NGC_Sgr_LMC_orbit(time, host_model_l, disk_params_h,\
+            relNGCSAG = NGC_Sgr_LMC_orbit(T, host_model_l, disk_params_h,\
             satellite_model_sgr2, LMC_model, pos[i], vel[i], sgr_pos, sgr_vel,\
             lmc_pos, lmc_vel)
+
             output_file('MWlLMC0Sgr_ICs{}.txt'.format(str(i)), time, posNGC, velNGC, posSag, velSag, relNGCSAG, posLMC = posLMC, velLMC = velLMC)
-            del(satellite_model_sgr2)
 
     elif sys.argv[1] == 'MWhSgrLMC':
-        satellite_model_sgr2 = ['NFW', 1E10, 44, 8]
 
-        for i in range(len(LMC_Ms)):
+        for i in range(len(pos)):
             time, posNGC, velNGC, posSag, velSag, posLMC, velLMC, \
-            relNGCSAG = NGC_Sgr_LMC_orbit(time, host_model_h, disk_params_l,\
-            satellite_model_sgr2, LMC_model,  pos_NGC2419, vel_NGC2419, sgr_pos, sgr_vel,\
+            relNGCSAG = NGC_Sgr_LMC_orbit(T, host_model_h, disk_params_l,\
+            satellite_model_sgr2, LMC_model, pos[i], vel[i], sgr_pos, sgr_vel,\
             lmc_pos, lmc_vel)
 
-            output_file('MWhLMC{}Sgr.txt'.format(str(i)), time, posNGC, velNGC, posSag, velSag, relNGCSAG, posLMC = posLMC, velLMC = velLMC)
+            output_file('MWhLMC0Sgr_ICs{}.txt'.format(str(i)), time, posNGC, velNGC, posSag, velSag, relNGCSAG, posLMC = posLMC, velLMC = velLMC)

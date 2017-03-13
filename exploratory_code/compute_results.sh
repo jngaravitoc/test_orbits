4398512e23bddc12f99000f808fa2ea4582c3756
jngaravitoc <jngaravitoc@email.arizona.edu>
2017-03-13 13:51:47 -0700

    script to run restuls

---
+++ exploratory_code/compute_results.sh
@@ -0,0 +1,4 @@
+python3 orbit_analysis.py 1 ../orbits/fix_sgr/lmc/ files_lmc results_fix_lmc
+python3 orbit_analysis.py 0 ../orbits/fix_sgr/no_lmc/ files_nolmc results_fix_nolmc
+python3 orbit_analysis.py 1 ../orbits/free_sgr/lmc/ files_lmc results_free_lmc
+python3 orbit_analysis.py 0 ../orbits/free_sgr/lmc/ files_nolmc results_free_nolmc
